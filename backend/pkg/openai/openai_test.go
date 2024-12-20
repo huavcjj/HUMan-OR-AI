858244451bcf5b2eb41d0dff28ae20b8eb99a80b
Mayu-Omichi <omichi.mayu@irl.sys.es.osaka-u.ac.jp>
2024-12-20 15:27:50 +0900

    openai.goとopenai_test.goの更新。お題の回答のみ生成する関数

--- backend/pkg/openai/openai_test.go
+++ backend/pkg/openai/openai_test.go
@@ -9,6 +9,35 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func TestGenerateAIAnswer(t *testing.T) {
+	// 相対パスで.envをロード
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		t.Fatalf("Failed to load .env file: %v", err)
+	}
+
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		t.Fatal("OPENAI_API_KEY not set")
+	}
+	//ここでお題を変えられる
+	topic:="学校が急に休みになった！なぜ？？"
+	oai := NewOpenAI(apiKey)
+	answer, err := oai.GenerateAIAnswer(context.Background(),topic)
+	if err != nil {
+		t.Fatalf("failed to generate: %v", err)
+	}
+
+	if topic == "" {
+		t.Error("odai is empty")
+	}
+	if answer == "" {
+		t.Error("answer is empty")
+	}
+	t.Logf("Odai: %s\nAnswer: %s\n", topic, answer)
+}
+//go test -v -run TestGenerateAIAnswer -count=1
+
 func TestGenerateGameTopicAIAnswer(t *testing.T) {
 	// 相対パスで.envをロード
 	err := godotenv.Load("../../.env")
